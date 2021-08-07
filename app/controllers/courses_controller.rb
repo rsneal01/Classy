0fae714c6215dc517c9667d20a117c4ed10d132e
rsneal01 <rsneal01@gmail.com>
2021-08-07 09:12:07 -0700

    added edit, update, and show functionality for Courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,19 +1,29 @@
 class CoursesController < ApplicationController
     def new
-        
         @teacher = current_user
         @course = Course.new
     end
 
     def create
-        binding.pry
+        # binding.pry
         @course = Course.create(course_params)
         redirect_to course_path(@course)
     end
     
     def show
+        @teacher = current_user
         @course = Course.find_by(id: params[:id])
         # logic to check if current user owns this course; if yes can edit course if no cannot edit
+    end
+
+    def edit
+        @course = Course.find_by(id: params[:id])
+    end
+
+    def update
+        course = Course.find_by(id: params[:id])
+        course.update(course_params)
+        redirect_to course_path(course)
     end
 
     def course_params
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
     # has_secure_password
     has_many :teachers, through: :courses
-    # has_many :courses
+    has_many :courses
     # has_secure_password
 end
