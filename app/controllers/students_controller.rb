de22c78ca39c1b462411843da14fb3c8c619e47f
rsneal01 <rsneal01@gmail.com>
2021-08-09 09:37:53 -0700

    created Students new and create actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,21 @@
 class StudentsController < ApplicationController
     def index
+        @students = Student.all
     end
 
     def new
+        @student = Student.new
     end
 
     def create
+        @student = Student.new(student_params)
     end
 
     def show
     end
 
+    def student_params
+        params.require(:student).permit(:name, :schedule)
+    end
 
 end
