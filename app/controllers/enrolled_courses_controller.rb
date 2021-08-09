c1f1c79870049fe4840128d135ce79ae6f8cd230
rsneal01 <rsneal01@gmail.com>
2021-08-09 09:56:53 -0700

    created EnrolledCourses new and create actions

--- app/controllers/enrolled_courses_controller.rb
+++ app/controllers/enrolled_courses_controller.rb
@@ -1,2 +1,14 @@
 class EnrolledCoursesController < ApplicationController
+    def new
+        @enrolled_course = EnrolledCourse.new
+    end
+
+    def create
+        
+    end
+
+    def show
+    end
+
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,15 @@ class StudentsController < ApplicationController
 
     def create
         @student = Student.new(student_params)
+        if @student.save
+            redirect_to student_path(@student)
+        else 
+            render 'new'
+        end
     end
 
     def show
+        @student = Student.find(params[:id])
     end
 
     def student_params
