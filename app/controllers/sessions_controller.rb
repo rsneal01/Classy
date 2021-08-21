30f7fc22135279f49c52c2095d2fb19068c46947
rsneal01 <rsneal01@gmail.com>
2021-08-21 14:12:06 -0700

    modified omniauth for github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,14 +12,24 @@ class SessionsController < ApplicationController
         # else
         #     redirect_to root_path
         # end
-        if @teacher = Teacher.find_by(name: params[:name])
-            session[:teacher_id] = @teacher.id
-            redirect_to teacher_path(@teacher)
-        # elsif @teacher = Teacher.find_by(id: params[:teacher_id])
+
+            auth = request.env["omniauth.auth"]
+            teacher = Teacher.where(:provider => auth['provider'],
+                              :uid => auth['uid'].to_s).first || Teacher.create_with_omniauth(auth)
+                              
+            reset_session
+            session[:teacher_id] = teacher.id
+            redirect_to root_path, :notice => 'Signed in!'
+        
+
+        # if @teacher = Teacher.find_by(name: params[:name])
         #     session[:teacher_id] = @teacher.id
-        else
-            redirect_to new_teacher_path
-        end
+        #     redirect_to teacher_path(@teacher)
+        # # elsif @teacher = Teacher.find_by(id: params[:teacher_id])
+        # #     session[:teacher_id] = @teacher.id
+        # else
+        #     redirect_to new_teacher_path
+        # end
     end
 
     def destroy
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,15 +2,25 @@ class Teacher < ApplicationRecord
     has_many :students, through: :enrolled_courses
     has_many :enrolled_courses
     has_many :courses
-    has_secure_password
-    validates :name, presence: true
-    validates :password, presence: true
-    validates :bio, length: { minimum: 5 }
+    # has_secure_password
+    # validates :name, presence: true
+    # validates :password, presence: true
+    # validates :bio, length: { minimum: 5 }
 
-    def self.find_or_create_from_auth(auth)
-        teacher = Teacher.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
-        teacher.name = auth['info']['name']
-        teacher.save
-        teacher
-    end
+    # def self.find_or_create_from_auth(auth)
+    #     teacher = Teacher.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+    #     teacher.name = auth['info']['name']
+    #     teacher.save
+    #     teacher
+    # end
+
+    def self.create_with_omniauth(auth)
+        create! do |teacher|
+          teacher.provider = auth['provider']
+          teacher.uid = auth['uid']
+          if auth['info']
+             teacher.name = auth['info']['name'] || ""
+          end
+        end
+      end
 end
