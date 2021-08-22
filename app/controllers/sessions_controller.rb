3b1415390ee8d96d42909e3d9771b710a98353b5
rsneal01 <rsneal01@gmail.com>
2021-08-22 09:30:41 -0700

    modified sessions create for using has secure password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if @teacher = Teacher.find_by(name: params[:name])
+        @teacher = Teacher.find_by(name: params[:name])
+        if @teacher && @teacher.authenticate(params[:password])
+           
             session[:teacher_id] = @teacher.id
             redirect_to teacher_path(@teacher)
-        # elsif @teacher = Teacher.find_by(id: params[:teacher_id])
-        #     session[:teacher_id] = @teacher.id
         else
             redirect_to new_teacher_path
         end
-            # auth = request.env["omniauth.auth"]
-            # teacher = Teacher.where(:provider => auth['provider'],
-            #                   :uid => auth['uid'].to_s).first || Teacher.create_with_omniauth(auth)
-                              
-            # reset_session
-            # session[:teacher_id] = teacher.id
-            # redirect_to root_path, :notice => 'Signed in!'
-        
     end
 
     def omni_create
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,10 +2,10 @@ class Teacher < ApplicationRecord
     has_many :students, through: :enrolled_courses
     has_many :enrolled_courses
     has_many :courses
-    # has_secure_password
+    has_secure_password
     validates :name, presence: true
-    # validates :password, presence: true
-    # validates :bio, length: { minimum: 5 }
+    validates :password, presence: true
+    validates :bio, length: { minimum: 5 }
 
     # def self.find_or_create_from_auth(auth)
     #     teacher = Teacher.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
